bef748e9cf9c1e516240037681a2437dd7421db7
alitu <alym11731996@gmail.com>
2024-03-09 15:00:11 +0300

    StepDefinitions

---
+++ src/test/java/StepDefinitions/CucumberStepDefinitions.java
@@ -0,0 +1,23 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CucumberStepDefinitions {
+    @Given("Udemy web sitesine git")
+    public void udemyWebSitesineGit() {
+    }
+
+    @When("Arama kutusuna tıkla")
+    public void aramaKutusunaTıkla() {
+    }
+
+    @When("Yazilim yaz ve ara")
+    public void yazilimYazVeAra() {
+    }
+
+    @Then("Yazılım kurslarının geldiğini kontrol et")
+    public void yazılımKurslarınınGeldiğiniKontrolEt() {
+    }
+}
